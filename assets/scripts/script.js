0059283b63ca7c919b2d3f918e3dcebf2c194b88
rocky2210 <lilavathra@gmail.com>
2024-08-06 21:57:19 +0530

    Education & Styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,3 +11,24 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+
+// To add event to all nav link
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+        for (let i = 0; i < pages.length; i++) {
+            if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+                pages[i].classList.add("active");
+                navLinks[i].classList.add("active");
+                window.scrollTo(0, 0);
+            } else {
+                pages[i].classList.remove("active");
+                navLinks[i].classList.remove("active");
+                }
+            }
+        });
+    }
