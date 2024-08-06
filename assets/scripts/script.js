ac581fa46358014bd23c95e0b677f3ed36f72d77
rocky2210 <lilavathra@gmail.com>
2024-08-06 18:33:19 +0530

    about page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,13 @@
 // Js
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
