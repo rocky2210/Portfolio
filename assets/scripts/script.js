bbc2194af2fb12407a52968262a15b966a6e9e28
rocky2210 <lilavathra@gmail.com>
2024-08-07 10:33:29 +0530

    Contact Form

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -32,3 +32,21 @@ for (let i = 0; i < navLinks.length; i++) {
             }
         });
     }
+
+
+// Contact form
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+for (let i = 0; i < formInputs.length; i++) {
+    formInputs[i].addEventListener("input", function () {
+
+      // check form validation
+    if (form.checkValidity()) {
+        formBtn.removeAttribute("disabled");
+    } else {
+        formBtn.setAttribute("disabled", "");
+    }
+    });
+}
