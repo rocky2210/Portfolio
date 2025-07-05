52d4dcc9d6381a7f3703e8500140d25adf1d6859
rocky2210 <lilavathra@gmail.com>
2025-07-05 04:27:37 +0000

    Added profile image

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,10 +5,7 @@ const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
-const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
 
 const navLinks = document.querySelectorAll("[data-nav-link]");
